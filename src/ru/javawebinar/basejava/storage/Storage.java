9d3a17dc4a8d1e6e3d7caa3106ed38d0491314f3
ym <wazap_by@mail.ru>
2021-05-25 23:28:03 +0200

    added Storage

--- src/ru/javawebinar/basejava/storage/Storage.java
+++ src/ru/javawebinar/basejava/storage/Storage.java
@@ -1,2 +1,20 @@
-package ru.javawebinar.basejava.storage;public interface Storage {
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.model.Resume;
+
+public interface Storage {
+
+    void clear();
+
+    void update(Resume r);
+
+    void save(Resume r);
+
+    Resume get(String uuid);
+
+    void delete(String uuid);
+
+    Resume[] getAll();
+
+    int size();
 }
