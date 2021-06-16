7b1ac95239cd5da945284655d372e3b3580eb50d
ym <wazap_by@mail.ru>
2021-06-16 13:06:24 +0200

    reflection

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -3,14 +3,21 @@ package ru.javawebinar.basejava;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
-        System.out.println(field.getName());
+        //System.out.println(field.getName());
         field.get(r);
-        System.out.println(r);
+        //System.out.println(r);
+
+        //Method method = r.getClass().getMethod("toString", null);
+        Method method = r.getClass().getDeclaredMethod("toString", null);
+        System.out.println(method.invoke(r));
+
     }
 }
