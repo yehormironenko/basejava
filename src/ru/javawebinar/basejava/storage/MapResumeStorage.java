0e95ee09b2776b411e9c58076d48bea8e84885fe
ym <wazap_by@mail.ru>
2021-07-19 23:57:19 +0200

    add generics

--- src/ru/javawebinar/basejava/storage/MapResumeStorage.java
+++ src/ru/javawebinar/basejava/storage/MapResumeStorage.java
@@ -7,38 +7,38 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MapResumeStorage extends AbstractStorage {
+public class MapResumeStorage extends AbstractStorage<Resume> {
 
     private Map<String, Resume> map = new HashMap<>();
 
     @Override
-    protected Object getSearchKey(String uuid) {
+    protected Resume getSearchKey(String uuid) {
         return map.get(uuid);
     }
 
     @Override
-    protected void doUpdate(Resume r, Object searchKey) {
+    protected void doUpdate(Resume r, Resume searchKey) {
         map.put(r.getUuid(), r);
     }
 
     @Override
-    protected boolean isExist(Object resume) {
+    protected boolean isExist(Resume resume) {
         return resume != null;
     }
 
     @Override
-    protected void doSave(Resume r, Object resume) {
+    protected void doSave(Resume r, Resume resume) {
         map.put(r.getUuid(), r);
     }
 
     @Override
-    protected Resume doGet(Object resume) {
-        return (Resume) resume;
+    protected Resume doGet(Resume resume) {
+        return resume;
     }
 
     @Override
-    protected void doDelete(Object searchKey) {
-        Resume resume = (Resume) searchKey;
+    protected void doDelete(Resume searchKey) {
+        Resume resume = searchKey;
         map.remove(resume.getUuid());
     }
 
--- src/ru/javawebinar/basejava/storage/MapUuidStorage.java
+++ src/ru/javawebinar/basejava/storage/MapUuidStorage.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 // TODO implement
 // TODO create new MapStorage with search key not uuid
-public class MapUuidStorage extends AbstractStorage {
+public class MapUuidStorage extends AbstractStorage<String> {
     private Map<String, Resume> map = new HashMap<>();
 
     @Override
@@ -18,27 +18,27 @@ public class MapUuidStorage extends AbstractStorage {
     }
 
     @Override
-    protected void doUpdate(Resume r, Object searchKey) {
-        map.replace((String) searchKey, r);
+    protected void doUpdate(Resume r, String searchKey) {
+        map.replace(searchKey, r);
     }
 
     @Override
-    protected boolean isExist(Object searchKey) {
+    protected boolean isExist(String searchKey) {
         return map.containsKey(searchKey);
     }
 
     @Override
-    protected void doSave(Resume r, Object searchKey) {
-        map.putIfAbsent((String) searchKey, r);
+    protected void doSave(Resume r, String searchKey) {
+        map.putIfAbsent(searchKey, r);
     }
 
     @Override
-    protected Resume doGet(Object searchKey) {
+    protected Resume doGet(String searchKey) {
         return map.get(searchKey);
     }
 
     @Override
-    protected void doDelete(Object searchKey) {
+    protected void doDelete(String searchKey) {
         map.remove(searchKey);
     }
 
